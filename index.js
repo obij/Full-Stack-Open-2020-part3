88922f98cdfa8c5da0d133a5165d915dfb2ad67e
Obinna Okafor <okaforobinna7@gmail.com>
2021-01-17 17:34:12 +0100

    Commit for exercise 3.6

--- index.js
+++ index.js
@@ -4,7 +4,13 @@ const morgan = require('morgan');
 const app = express()
 app.use(express.json())
 
-app.use(morgan('tiny'));
+//app.use(morgan('tiny'));
+
+morgan.token('person', (request, response) => {
+  return JSON.stringify(request.body)
+})
+
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :person'))
 
 
 let persons = [
